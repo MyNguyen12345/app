82bb4b7755824d537a245d5373fa3a535efa7cf4
tritran14 <tranvantri2000@gmail.com>
2022-03-30 23:04:22 +0700

    update CustomerAccount

--- src/main/java/com/cnpm/workingspace/constants/ErrorCode.java
+++ src/main/java/com/cnpm/workingspace/constants/ErrorCode.java
@@ -7,7 +7,8 @@ public enum ErrorCode {
     SOMETHING_WRONG("001","Something wrong !"),
     SUCCESS("200","Success !"),
     ERROR("002","Error !"),
-    UNAUTHENTICATED("401", "Unauthorized");
+    UNAUTHENTICATED("401", "Unauthorized"),
+    USERNAME_ALREADY_EXISTS("402","Username already exists");
     public final String errorCode;
     public final String msg;
 }
--- src/main/java/com/cnpm/workingspace/dto/CustomerAccount.java
+++ src/main/java/com/cnpm/workingspace/dto/CustomerAccount.java
@@ -3,28 +3,99 @@ package com.cnpm.workingspace.dto;
 import com.cnpm.workingspace.model.Account;
 import com.cnpm.workingspace.model.Customer;
 
+import java.util.Date;
+
 public class CustomerAccount {
-    private Account account;
-    private Customer customer;
+    private String username;
+    private String password;
 
-    public CustomerAccount(Account account, Customer customer) {
-        this.account = account;
-        this.customer = customer;
+    private String email;
+    private String customerName;
+    private String citizenId;
+    private Date birthday;
+    private String nationality;
+    private String phoneNumber;
+
+    public CustomerAccount(String username, String password, String email, String customerName, String citizenId, Date birthday, String nationality, String phoneNumber) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.customerName = customerName;
+        this.citizenId = citizenId;
+        this.birthday = birthday;
+        this.nationality = nationality;
+        this.phoneNumber = phoneNumber;
     }
 
-    public Account getAccount() {
-        return account;
+    public String getUsername() {
+        return username;
     }
 
-    public void setAccount(Account account) {
-        this.account = account;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public Customer getCustomer() {
-        return customer;
+    public String getPassword() {
+        return password;
     }
 
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCitizenId() {
+        return citizenId;
+    }
+
+    public void setCitizenId(String citizenId) {
+        this.citizenId = citizenId;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Account getAccount(){
+        return new Account(username,password);
+    }
+
+    public Customer getCustomer(){
+        return new Customer(email,customerName,citizenId,birthday,nationality,phoneNumber);
     }
 }
