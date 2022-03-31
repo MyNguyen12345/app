da5ee4e6c5b3d6892bb3c643618f89a129ea10b5
Thai Luc <thaitangluc2412@gmail.com>
2022-04-01 00:19:06 +0700

    add price

---
+++ src/main/java/com/cnpm/workingspace/service/PriceServiceImpl.java
@@ -0,0 +1,53 @@
+package com.cnpm.workingspace.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cnpm.workingspace.model.Price;
+import com.cnpm.workingspace.repository.PriceRepository;
+
+@Service
+public class PriceServiceImpl implements PriceService {
+
+	@Autowired
+    private PriceRepository priceRepository;
+
+	@Override
+	public List<Price> getAll() {
+		return priceRepository.findAll();
+	}
+
+	@Override
+	public void insertPrice(Price price) {
+		priceRepository.save(price);
+	}
+
+	@Override
+	public boolean updatePerson(Price price, int id) {
+		Optional<Price> curPrice = getPriceById(id);
+		if (curPrice.isPresent()) {
+			Price curPriceNew = curPrice.get();
+			curPriceNew.setHourPrice(price.getHourPrice());
+            curPriceNew.setDayPrice(price.getDayPrice());
+            curPriceNew.setWeekPrice(price.getWeekPrice());
+            curPriceNew.setMonthPrice(price.getMonthPrice());
+            priceRepository.save(curPriceNew);
+            return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void deletePerson(int id) {
+		priceRepository.deleteById(id);
+	}
+	
+	@Override
+	public Optional<Price> getPriceById(int id) {
+		return priceRepository.findById(null);
+	}
+	
+}
