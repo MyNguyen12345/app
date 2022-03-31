808f775aac78cacc885be16b9227856cceaab6cf
Thai Luc <thaitangluc2412@gmail.com>
2022-04-01 00:01:41 +0700

    fix bug price

--- src/main/java/com/cnpm/workingspace/security/filter/CustomAuthorizationFilter.java
+++ src/main/java/com/cnpm/workingspace/security/filter/CustomAuthorizationFilter.java
@@ -34,7 +34,7 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         if(request.getServletPath().startsWith("/api/auth/")
-        		||request.getServletPath().startsWith("/api/luc/")
+        		||request.getServletPath().startsWith("/api/price/")
         		){
             System.out.println("path (pass): "+request.getServletPath());
             filterChain.doFilter(request,response);
